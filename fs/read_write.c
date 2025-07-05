512d894000bf48f9afaffce5b9b6b42f6624576b
angelomds42 <angelomds42@gmail.com>
2025-07-05 12:39:35 -0300

    KernelSU: Implement Implement scope-minimized manual hooks v1.6
    Implemented hooks:
     - do_execve
     - sys_faccessat
     - sys_read
     - sys_newfstatat (stat)
     - input_event for safemode
    
    Reference: https://github.com/backslashxx/KernelSU/issues/5#issue-2872855633
    
    Change-Id: I94cc26677986328dfcbcf44b183b7aab6159fad6
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -607,6 +607,37 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
+			size_t *count_ptr);
+#endif
+
+SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
+{
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_read_hook)) 
+		ksu_handle_sys_read(fd, &buf, &count);
+#endif
+	return ksys_read(fd, buf, count);
+}
+
+ssize_t ksys_write(unsigned int fd, const char __user *buf, size_t count)
+{
+	struct fd f = fdget_pos(fd);
+	ssize_t ret = -EBADF;
+
+	if (f.file) {
+		loff_t pos = file_pos_read(f.file);
+		ret = vfs_write(f.file, buf, count, &pos);
+		if (ret >= 0)
+			file_pos_write(f.file, pos);
+		fdput_pos(f);
+	}
+
+	return ret;
+}
+
 SYSCALL_DEFINE3(write, unsigned int, fd, const char __user *, buf,
 		size_t, count)
 {
